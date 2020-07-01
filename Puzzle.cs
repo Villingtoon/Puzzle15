6885e043026114a271abb3e33635316877686190
Noob gamer <legofanslv@gmail.com>
2020-07-01 19:34:27 +0300

    ver 19:34

--- Puzzle.cs
+++ Puzzle.cs
@@ -23,15 +23,29 @@ namespace Puzzle15
         {
             this.BackColor = Color.Lime;
             this.Text = "Puzzle15";
+            this.ClientSize = new Size(500, 500);
         }
 
         private void AddButtons()
         {
-            Button button;
-            for(int i = 1; i < 16; i++)
+            int blockCount = 1;
+            PuzzleBlock block;
+            for(int row = 1; row < 5; row++)
             {
-                button = new Button();
-                this.Controls.Add(button);
+                for (int col = 1; col < 5; col++)
+                {
+                    block = new PuzzleBlock();
+                    block.Top = row * 85;
+                    block.Left = col * 85;
+                    block.Text = blockCount.ToString();
+                    if(blockCount == 16)
+                    {
+                        block.Text = string.Empty;
+                        block.BackColor = Color.Lime;
+                    }
+                    blockCount++;
+                    this.Controls.Add(block);
+                }
             }
         }
     }
